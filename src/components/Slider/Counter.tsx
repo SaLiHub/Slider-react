9577ba470e04adff171f1439a10b3f2f8f724527
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-11-06 14:15:22 +0200

    Add Counter Component

---
+++ src/components/Slider/Counter.tsx
@@ -0,0 +1,12 @@
+// interface Props {
+//   config: {
+//     counter: boolean;
+//   }
+// }
+
+function Counter():JSX.Element {
+  // const { counter } = config;
+  return <span className="slider__counter">12</span>;
+}
+
+export default Counter;
