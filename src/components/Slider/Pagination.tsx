7c62c94cc82cb8b4ba5dc84ef3adf85a94df51e3
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-11-08 20:19:01 +0200

    Add pagination functionality

--- src/components/Slider/Pagination.tsx
+++ src/components/Slider/Pagination.tsx
@@ -1,20 +1,32 @@
+import { Dispatch, SetStateAction } from 'react';
+
 interface Props {
   direction: string;
   slidesLength: number;
+  setActiveSlide: Dispatch<SetStateAction<number>>;
+  activeSlide: number;
 }
 
 function Pagination(props: Props): JSX.Element {
-  const { slidesLength, direction } = props;
+  const {
+    slidesLength, direction, setActiveSlide, activeSlide,
+  } = props;
   const paginationState = direction === 'vertical' ? 'is-vertical' : 'is-horizontal';
   const bullets = [];
+
   for (let i = 0; i < slidesLength; i++) {
+    let className = '';
+    if (i === activeSlide) className = 'slider__bullet is-active';
+    else className = 'slider__bullet';
+
     bullets.push(
       <button
         type="button"
-        className="slider__bullet"
+        className={className}
         aria-label={`select ${i} slide`}
         data-index={i}
         key={i}
+        onClick={() => setActiveSlide(i)}
       />,
     );
   }
