2f04fbc7a3751a12c85f38aed912230fddcd933c
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-11-08 19:10:11 +0200

    Refactor code and add disabled state

--- src/components/Slider/Arrows.tsx
+++ src/components/Slider/Arrows.tsx
@@ -1,23 +1,23 @@
 interface Props {
-  changeSliderPosition: (i: number) => void;
+  setActiveSlide: (i: number) => void;
   activeSlide: number;
   slidesLength: number;
 }
 
 function Arrows(props: Props): JSX.Element {
+  const { activeSlide, setActiveSlide, slidesLength } = props;
+
   function handleArrowClick(e: React.MouseEvent<HTMLElement>) {
-    const { activeSlide, changeSliderPosition, slidesLength } = props;
     const target = e.target as HTMLTextAreaElement;
 
     if (target.classList.contains('slider__arrow_back')) {
-      if (activeSlide <= 0) return;
+      if (activeSlide === 0) return;
       const newIndex = activeSlide - 1;
-      changeSliderPosition(newIndex);
+      setActiveSlide(newIndex);
     } else if (target.classList.contains('slider__arrow_forward')) {
-      console.log(activeSlide);
-      if (activeSlide >= slidesLength - 1) return;
+      if (activeSlide === slidesLength - 1) return;
       const newIndex = activeSlide + 1;
-      changeSliderPosition(newIndex);
+      setActiveSlide(newIndex);
     }
 
     // function triggerArrows(newIndex: number) {
@@ -25,19 +25,28 @@ function Arrows(props: Props): JSX.Element {
     //   applyChangesAfterTransition(newIndex);
     // }
   }
+  if (activeSlide === 0) {
+    var isBackDisabled = true;
+    console.log('first');
+  } else if (activeSlide === slidesLength - 1) {
+    console.log('last');
+    var isForwardDisabled = true;
+  }
   return (
     <>
-      <button
-        type="button"
-        aria-label="forward"
-        className="slider__arrow slider__arrow_forward"
-        onClick={handleArrowClick}
-      />
       <button
         type="button"
         aria-label="back"
         className="slider__arrow slider__arrow_back"
         onClick={handleArrowClick}
+        disabled={isBackDisabled}
+      />
+      <button
+        type="button"
+        aria-label="forward"
+        className="slider__arrow slider__arrow_forward"
+        onClick={handleArrowClick}
+        disabled={isForwardDisabled}
       />
     </>
   );
