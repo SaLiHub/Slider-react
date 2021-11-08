8b3e46d7e2f25da193889bc7a0f45c425e4fd9ce
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-11-08 19:09:10 +0200

    Add active slide

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,10 @@ import * as ReactDOM from 'react-dom';
 import Slider from './components/Slider/Slider';
 
 ReactDOM.render(
-  <Slider navigation={{ arrows: true, pagination: true }} counter={true} />,
+  <Slider
+    navigation={{ arrows: true, pagination: true }}
+    counter={true}
+    activeSlide={0}
+  />,
   document.getElementById('slider1'),
 );
