c8f3dacdda01240a6d9f956fb8c89ecdf9bd4e80
Oleksandr Lishchynskyi <sashalischinskiy72@gmail.com>
2021-11-11 22:22:40 +0200

    Refactor code

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,30 @@
 import './index.sass';
 import * as ReactDOM from 'react-dom';
 import Slider from './components/Slider/Slider';
+import Slide from './components/Slider/Slide';
 
 ReactDOM.render(
   <Slider
     navigation={{ arrows: true, pagination: true }}
     counter={true}
-    activeSlide={0}
-  />,
-  document.getElementById('slider1'),
+    initialActiveSlide={0}
+    className="slider"
+  >
+    <Slide>
+      <button type="button">a11y</button>1
+    </Slide>
+    <Slide>
+      <button type="button">a11y</button>2
+    </Slide>
+    <Slide>
+      <button type="button">a11y</button>3
+    </Slide>
+    <Slide>
+      <button type="button">a11y</button>4
+    </Slide>
+    <Slide>
+      <button type="button">a11y</button>5
+    </Slide>
+  </Slider>,
+  document.getElementById('slider-container'),
 );
